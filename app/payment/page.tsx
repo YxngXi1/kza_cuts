31aba2c8b86e765e33ded99f778be1308c46d0f3
YxngXi1 <yang@xue.email>
2024-03-11 20:19:38 -0400

    finished payment (no back-end yet)

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -2,19 +2,24 @@ import Image from "next/image";
 import Link from 'next/link';
 
 import { Hero } from "@/components"
-import { Poppins } from "next/font/google"
 
 export default function Payment() {
   return (
     <main>
         <h2 className="text-center text-6xl">Thanks for getting a cut!</h2>
         <h3 className="text-center text-4xl mt-10">How would you like to pay?</h3>
-        <div className="flex justify-center space-between bottom-0 absolute outline">
-          <div className="ml-auto mr-auto outline">
-            <p>Cash (in-person)</p>
+        <div className="flex bottom-0 w-full absolute mb-36">
+          <div className="ml-auto mr-auto items-center flex-col flex">
+            <p className="text-4xl">Cash (in-person)</p>
+            <Link href='/cash'>
+                <button className="payment_button mt-8">Click!</button>
+            </Link>
           </div>
-          <div className="ml-auto mr-auto outline">
-            <p>Online</p>
+          <div className="ml-auto mr-auto items-center flex-col flex">
+            <p className="text-4xl">Online (paypal)</p>
+            <Link href="/paypal">
+                <button className="payment_button mt-8">Click!</button>
+            </Link>
           </div>
         </div>
     </main>
