e02538453f8bcffd24fd60b5f13987b83925e625
YxngXi1 <yang@xue.email>
2024-03-11 20:19:08 -0400

    next 2 portions

---
+++ app/cash/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from 'next/link';
+
+import { Hero } from "@/components"
+
+export default function Payment() {
+  return (
+    <main>
+        <p>cash</p>
+    </main>
+  );
+}
---
+++ app/paypal/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from 'next/link';
+
+import { Hero } from "@/components"
+
+export default function Payment() {
+  return (
+    <main>
+        <p>paypal</p>
+    </main>
+  );
+}
